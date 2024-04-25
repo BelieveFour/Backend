51c19603d21e23c9d19875372795ccb20a4ff0bd
Kiran <kiran@intellispiders.com>
2024-04-25 18:01:10 +0530

    updated code

---
+++ README.md
@@ -0,0 +1,2 @@
+# TyreBackendCode
+# TyreBackendCode
---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config();
+
+
+const mySqlPool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: parseInt(process.env.MYSQL_PORT, 10)
+});
+
+
+export default mySqlPool;
