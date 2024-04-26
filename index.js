ea053e7885f818a0f763be647f34cd150b1a9df7
Kiran <kiran@intellispiders.com>
2024-04-26 12:28:58 +0530

    updated code

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ mySqlPool.query('SELECT 1').then(() => {
 app.get("/", async (req, res) => {
   try {
     const [rows, fields] = await mySqlPool.query("SELECT * FROM chart");
-    res.json({ persons: rows });
+    res.json({ product: rows });
   } catch (error) {
     console.error('Error querying database:', error);
     res.status(500).json({ error: 'Internal server error' });
